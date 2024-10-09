e4a31e7f05ae24d5a259d9aa98b09a147c1b0506
Mohahamed99-by <mohamedtolba161@gmail.com>
2024-10-09 15:04:44 +0200

    first commit5

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-// src/App.js
-// import React, { useState } from 'react';
-// import BasicComponent from './BasicComponent/BasicComponent';
-
+import React from 'react';
 import IPTVServicesTemplate from "./IPTVServicesTemplate/IPTVServicesTemplate";
 
-// import ToDoApp from "./ToDoApp/ToDoApp";
-
-// import CompenentCounter from "./CompenentCounter/CompenentCounter";
-
-
 const App = () => {
-  // const [name, setName] = useState('Mohamed')
   return (
-    // <BasicComponent name={name} />
-    // <CompenentCounter />
-    // <ToDoApp />
-    <IPTVServicesTemplate />
+    <IPTVServicesTemplate basename="/iptv" />
   );
 };
 
